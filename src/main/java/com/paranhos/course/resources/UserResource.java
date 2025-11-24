d757b5c0711a33a26ef8d5db7c78c32928bf0744
cledson-paranhos <cledsonvgp@hotmail.com>
2025-11-24 17:48:13 -0300

    add class User/UserResources

---
+++ src/main/java/com/paranhos/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.paranhos.course.resources;
+
+import com.paranhos.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Maria", "Maria@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(user);
+    }
+}
